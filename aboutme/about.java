2aac0f604a2c32f4fee8cdd2e8384441aaceb11a
sudoes <hafiz.jefri@gmail.com>
2013-07-08 02:42:38 +0800

    Aboutme

---
+++ aboutme/about.java
@@ -0,0 +1,6 @@
+/* Hello World Java Program */
+class about  {
+        public static void main(String[] args) {
+                    System.out.println("\nName\t\t:\tYour Name Here\nAge\t\t:\t1900 Years Old\nReg. Num.\t:\t01DNS13F1000\nLast School\t:\tMIT");
+                        }
+}
