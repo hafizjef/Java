40b440c2a0014565365a1f3fd646b51c36b08503
Hafiz Jefri <hafiz.jefri@gmail.com>
2013-09-20 10:51:16 +0800

    test

--- ATM/atm.java
+++ ATM/atm.java
@@ -4,85 +4,104 @@ class atm
 {
 	public static void main(String args[]) throws IOException
 	{
+		
 		String money;
 		BufferedReader getinput = new BufferedReader(new InputStreamReader(System.in));
-		
 		System.out.println("\n\n+Welcome to Maybank2Mu berhad, This machine can spit out :\n\n\t| 10, 20, 50 and 100 |");
-		System.out.println("\n\n+Please input amount of money that you want to withdraw(Min. RM 10) : ");
 		
-		money = getinput.readLine();
 		
-		int amount = Integer.parseInt(money), note100=0, note50=0, note20=0, note10=0;
-		int max100=10, max50=10, max20=10, max10=10;
+		int max100=10, max50=10, max20=10, max10=10, note100=0, note50=0, note20=0, note10=0;
 		
-		int total=amount;
-		
-		if (amount<10)
-		{
-			System.out.println("\n\nThe entered value is invalid");
-			return;
-		}
 			
-		
-		
-		if (amount>=100)
+		int loop=0;
+		while (loop<5)
 		{
-			note100=amount/100;
-			amount=amount%100;
-			if (note100>max100)
-			{
-				amount=(amount%100)+(100*(note100-max100));
-				note100=max100;
-				System.out.println(amount);
-			}
 			
-		}
-		if (amount>=50)
-		{
-			note50=amount/50;
-			amount=amount%50;
-			if (note50>max50)
+			System.out.print("\n\n+Please input amount of money that you want to withdraw(Min. RM 10) : ");
+		
+			money = getinput.readLine();
+			int amount = Integer.parseInt(money); 
+			
+			int total=amount;
+		
+			if (amount<10)
 			{
-				amount=(amount%50)+(50*(note50-max50));
-				note50=max50;
-				System.out.println(amount);
+				System.out.println("\n\nThe entered value is invalid");
+				continue; //continue to loop, use return to end;
 			}
-		}
-		if (amount>=20)
-		{
-			note20=amount/20;
-			amount=amount%20;
-			if (note20>max100)
+			else if (amount>1500)
 			{
-				amount=(amount%20)+(20*(note20-max20));
-				note20=max20;
-				System.out.println(amount);
+				System.out.println("\n\nYou have excedeed the maximum amount");
+				return;
 			}
-		}
-		if (amount>=10)
-		{
-			note10=amount/10;
-			amount=amount%10;
-			if (note10>max10)
+
+
+
+			if (amount>=100)
 			{
-				amount=(amount%10)+(10*(note10-max10));
-				note10=max10;
-				System.out.println(amount);
-				System.out.println("\n\nSorry, our machine is out of service");
-				System.exit(0);
+				note100=amount/100;
+				amount=amount%100;
+				if (note100>max100)
+				{
+					amount=(amount%100)+(100*(note100-max100));
+					note100=max100;
+					System.out.println(amount);
+				}
+				max100=max100-note100;
 			}
+			if (amount>=50)
+			{
+				note50=amount/50;
+				amount=amount%50;
+				if (note50>max50)
+				{
+					amount=(amount%50)+(50*(note50-max50));
+					note50=max50;
+					System.out.println(amount);
+				}
+				max50=max50-note50;
+			}
+			if (amount>=20)
+			{
+				note20=amount/20;
+				amount=amount%20;
+				if (note20>max100)
+				{
+					amount=(amount%20)+(20*(note20-max20));
+					note20=max20;
+					System.out.println(amount);
+				}
+				max20=max20-note20;
+			}
+			if (amount>=10)
+			{
+				note10=amount/10;
+				amount=amount%10;
+				if (note10>max10)
+				{
+					amount=(amount%10)+(10*(note10-max10));
+					note10=max10;
+					System.out.println(amount);
+					System.out.println("\n\nSorry, our machine is out of service");
+					System.exit(0);
+				}
+				max10=max10-note10;
+			}
+		
+			if (amount!=0)
+			{
+				System.out.println("Error!, Please input amount in multiple of 10");
+				return;
+			}
+			else
+			{
+				System.out.println("\n\nYou gonna withdraw RM "+total+" from your account");
+				System.out.println("\n\nNote $100\t= "+note100+"\nNote $50\t= "+note50+"\nNote $20\t= "+note20+"\nNote $10\t= "+note10);
+				loop=loop+1;
+			}
+		
 		}
 		
-		if (amount!=0)
-		{
-			System.out.println("Error!, Please input amount in multiple of 10");
-			return;
-		}
-		else
-		{
-			System.out.println("\n\nYou gonna withdraw RM "+total+" from your account");
-			System.out.println("\n\nNote $100\t= "+note100+"\nNote $50\t= "+note50+"\nNote $20\t= "+note20+"\nNote $10\t= "+note10);
-		}
 		
 	}
 }
