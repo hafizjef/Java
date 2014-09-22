dcb1de7e612fb9e90bb820690f92eae5c6e047f6
Hafiz Jefri <hafiz.jefri@gmail.com>
2014-09-22 12:22:26 +0800

    Update Student Mark.java

--- Semester 3/Java Class/Student Mark.java
+++ Semester 3/Java Class/Student Mark.java
@@ -1,4 +1,7 @@
-import java.io.*;
+/*
+* Title  : Class Example
+* Author : Hafiz Jefri
+*/
 
 class student{
 	String sname;
@@ -13,14 +16,20 @@ class student{
 	}
 
 	public void showMark(){
-		System.out.println("Name : " + sname);
-		System.out.println("History Mark : " + hmark);
-		System.out.println("Math Mark : " + mmark);
-		System.out.println("English Mark : " + engmark);
+	System.out.println("\nName\t\t: " + sname);
+		System.out.println("History Mark\t: " + hmark);
+		System.out.println("Math Mark\t: " + mmark);
+		System.out.println("English Mark\t: " + engmark);
 	}
 
 	public static void main (String args[]){
 		student John = new student("John", 80, 90, 100);
 		John.showMark();
+
+		//example
+		student Cupok = new student("Cupok", 10, 20, 30);
+		student Sotong = new student("Sotong", 20, 30, 40);
+		Cupok.showMark();
+		Sotong.showMark();
 	}
 }
